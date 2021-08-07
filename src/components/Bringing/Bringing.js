4376761866b0e7b3a6d3c573d5f5b62e52b8adbb
Dewi Anggraini <goddess.guy@gmail.com>
2021-08-07 16:49:26 +0800

    add Bringing

---
+++ src/components/Bringing/Bringing.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Bringing = () => {
+    return (
+        <div>
+            <h1>BRINGING YOU THE BEST AUDIO GEAR</h1>
+            <p>Located at the heart of New York City, Audiophile is the premier store for high end headphones,
+  earphones, speakers, and audio accessories. We have a large showroom and luxury demonstration
+  rooms available for you to browse and experience a wide range of our products. Stop by our store
+  to meet some of the fantastic people who make Audiophile the best place to buy your portable
+  audio equipment.</p>
+        </div>
+        
+    )
+}
+
+export default Bringing
