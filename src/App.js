68538e9d3ba3485301b385e95a05cde7ae5a599a
Dewi Anggraini <goddess.guy@gmail.com>
2021-08-07 15:45:06 +0800

    comment sementara logo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
-import logo from './logo.svg';
+// import logo from './logo.svg';
 
-=======
-import logo from "./logo.svg";
 import categoryEarphone from "../src/components/Category-Earphone/categoryEarphone";
->>>>>>> bf076ea1fb107bc63ffcfabc040de32210d752ab
 
 function App() {
   return (
