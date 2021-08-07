bebf9657740de2930a4f3fe1e2613b41b51f55a7
Jedidiah Judah Marada <jedidiahjudahmarada@hotmail.com>
2021-08-07 17:26:20 +0700

    mulai buat

---
+++ src/layouts/ProductDetail-Speaker/PDSpeaker.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import xx99M2 from '../../assets/product-xx99-mark-two-headphones/desktop/image-product.jpg'
+
+const PdSpeaker = () => {
+  return (
+    <div>
+      <img src={xx99M2} />
+    </div>
+  );
+};
+
+export default PdSpeaker;
