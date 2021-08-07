b864a1796302ccf5125fc1d369988783aeb73781
Dewi Anggraini <goddess.guy@gmail.com>
2021-08-07 16:49:08 +0800

    add HomePage

---
+++ src/layouts/HomePage/homePage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Bringing from '../../components/Bringing/Bringing'
+
+const homePage = () => {
+    return (
+        <div>
+            <Bringing/>
+            
+        </div>
+    )
+}
+
+export default homePage
